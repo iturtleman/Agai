29cf33622623bc2dd868423ee1138137d9e4057c
Ivan Hernandez <iturtleman128@gmail.com>
2024-01-14 00:29:56 -0800

    clean things up a bit

---
+++ Core/Agai/EnergyFlowGrid.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Agai;
+
+public record class EnergyFlowGrid(string Name, int Width, int Height, int Version)
+{
+    public EnergyFlowPanel?[,] Panels = new EnergyFlowPanel[Width,Height];
+}
